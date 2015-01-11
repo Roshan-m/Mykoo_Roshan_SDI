cea78ca1d3cdf7b7133f3d189112d3394ec82c9f
Roshan-m <rjmykoo@fullsail.edu>
2015-01-11 00:45:06 -0500

    Problem 3 finished

--- day3LabExpression/js/script.js
+++ day3LabExpression/js/script.js
@@ -15,7 +15,8 @@ numberOfPies= 5;
 
 var numberOfSlicesPerPerson;
 numberOfSlicesPerPerson =(slicesPerPizza*numberOfPies)/7;//Equation
-console.log("Each person ate"+" "+numberOfSlicesPerPerson.toFixed(2)+" "+"slices of pizza at the party")//Answer
+console.log("Each person ate"+" "+numberOfSlicesPerPerson.toFixed(2)+" "+"slices of pizza at the party");
+//Answer
 
 //Problem 1 : Slice	of	Pie part	II;
 var slicesPerPizza;
@@ -26,7 +27,7 @@ var numberOfPies;
 numberOfPies= 5;
 
 var slicesPerPerson;
-slicesPerPerson= 5.71
+slicesPerPerson= 5.71;
 
 var numberOfSlicesForSparky;
 numberOfSlicesForSparky=(slicesPerPizza*numberOfPies)/slicesPerPerson-peopleAtParty;//Equation
@@ -52,3 +53,26 @@ averageWeeklySpent = (shoppingDay1+shoppingDay2+shoppingDay3+shoppingDay4+shoppi
 var totalWeeklySpent;                                                                       // Equations
 totalWeeklySpent= shoppingDay1 + shoppingDay2 + shoppingDay3 + shoppingDay4 + shoppingDay5; //Answer
 console.log("You have spent a total	of"+" $"+totalWeeklySpent+" "+"on groceries over 5 weeks. That is an average of"+" $"+averageWeeklySpent+" "+"per week");
+
+
+//Problem 3: Discounts
+//
+var originalPrice;
+originalPrice = 60;
+
+var discountPercent;
+discountPercent = (25/100);
+
+var descriptionOfItem;
+descriptionOfItem = "Video Game";
+
+var salesTaxPercentage;
+salesTaxPercentage = (9/100);
+
+var priceWithDiscount;
+priceWithDiscount = originalPrice - (originalPrice * discountPercent);
+
+var priceWithTax;
+priceWithTax = priceWithDiscount + (priceWithDiscount*salesTaxPercentage);
+
+console.log("Your"+" "+descriptionOfItem+"was originally"+" $"+originalPrice+" "+"but after a"+" "+25+"% "+"discount, it is now"+" $"+(priceWithDiscount)+" "+"without tax, and"+" $"+(priceWithTax)+" "+"with tax");
