8350e1ebf8c77080eb94aa5d0fcf08ff60719125
Roshan-m <rjmykoo@fullsail.edu>
2015-01-11 00:16:28 -0500

    Problem 2 average shopping bill
    finished variables equations and solution

--- day3LabExpression/js/script.js
+++ day3LabExpression/js/script.js
@@ -33,3 +33,22 @@ numberOfSlicesForSparky=(slicesPerPizza*numberOfPies)/slicesPerPerson-peopleAtPa
 
 console.log("Sparky	got"+" "+numberOfSlicesForSparky.toFixed(4)+" "+"slices of pizza.");//Answer
 
+//Problem 2: Average shopping bill
+//5 Shopping Days
+var shoppingDay1;
+shoppingDay1 = 101.75;
+var shoppingDay2;
+shoppingDay2 = 35.00;
+var shoppingDay3;
+shoppingDay3 = 150.00;
+var shoppingDay4;
+shoppingDay4 = 20.50;
+var shoppingDay5;
+shoppingDay5 = 34.00;
+
+var averageWeeklySpent;
+averageWeeklySpent = (shoppingDay1+shoppingDay2+shoppingDay3+shoppingDay4+shoppingDay5)/5;
+
+var totalWeeklySpent;                                                                       // Equations
+totalWeeklySpent= shoppingDay1 + shoppingDay2 + shoppingDay3 + shoppingDay4 + shoppingDay5; //Answer
+console.log("You have spent a total	of"+" $"+totalWeeklySpent+" "+"on groceries over 5 weeks. That is an average of"+" $"+averageWeeklySpent+" "+"per week");
