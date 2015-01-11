dc0bb15dd5558ba40ae8e89f3c525d29ae15663d
Roshan-m <rjmykoo@fullsail.edu>
2015-01-10 23:37:10 -0500

    Problem 1 part 1 complete
    console.log’d the proper statement need to answer the question

--- day3LabExpression/js/script.js
+++ day3LabExpression/js/script.js
@@ -3,4 +3,18 @@
  1/10/2015
  MDV2330L - Lab Instructional Assignment: Expressions
  */
-console.log('Roshan Mykoo \n 1/10/15')
+console.log('Roshan Mykoo \n 1/10/15');
+
+//Problem 1 : Slice	of	Pie	part	1;
+var slicesPerPizza;
+slicesPerPizza= 8;
+var peopleAtParty;
+peopleAtParty= 7;
+var numberOfPies;
+numberOfPies= 5;
+
+var numberOfSlicesPerPerson;
+numberOfSlicesPerPerson =(slicesPerPizza*numberOfPies)/7;
+console.log("Each person ate"+" "+numberOfSlicesPerPerson+" "+"slices of pizza at the party")
+
+//
