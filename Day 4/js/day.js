91e9d90a226deeec952cd40685c563d6afa4dbca
Roshan-m <rjmykoo@fullsail.edu>
2015-01-13 18:14:13 -0500

    Started working with arrays and the avengers

--- Day 4/js/day.js
+++ Day 4/js/day.js
@@ -5,4 +5,32 @@ SDI Campus
 Day 4 In class assignment
  */
 
-alert("test")
+//alert("test")
+
+//Let's create a basic array
+
+var avengersNames = ["Thor","Hulk","Captain America","Iron Man"];
+console.log(avengersNames);
+
+console.log(avengersNames[1]);
+
+//Switch a team member - iron man for spiderman
+avengersNames[2] ="Spiderman";
+console.log(avengersNames);
+
+//Add team member
+avengersNames[4] = "Black Widow";
+console.log(avengersNames);
+
+// use a variable as a index#
+var num = 2;
+console.log(avengersNames[num]);
+
+//Length property
+//Dot syntax - Fancy for use a period
+console.log(avengersNames.length);
+
+//Last index number is the length -1 of an array
+
+avengersNames[avengersNames.length] = "Powerman";
+console.log(avengersNames);
