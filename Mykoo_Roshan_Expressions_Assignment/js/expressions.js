f8f3d8e8f122edf34462e5f7fdb0873792d26c7e
Roshan-m <rjmykoo@fullsail.edu>
2015-01-13 22:48:17 -0500

    Created expression for area of triangle

--- Mykoo_Roshan_Expressions_Assignment/js/expressions.js
+++ Mykoo_Roshan_Expressions_Assignment/js/expressions.js
@@ -7,4 +7,10 @@ Expressions Assignment.
 
 //alert("test");
 
+var height = prompt("We are calculating the area of a triangle.\n Please provide one Height of a triangle");
+var base = prompt("Please provide a base for triangle");
+
+var areaTriangle = 1/2*(base*height);
+ console.log(areaTriangle)
+
  
