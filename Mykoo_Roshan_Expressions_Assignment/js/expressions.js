385584f78085712959c8ffb33f3f9c136c45f02e
Roshan-m <rjmykoo@fullsail.edu>
2015-01-14 00:15:57 -0500

    Final Code correction

--- Mykoo_Roshan_Expressions_Assignment/js/expressions.js
+++ Mykoo_Roshan_Expressions_Assignment/js/expressions.js
@@ -9,24 +9,27 @@ Expressions Assignment.
 
 
  //User prompts asking for the sides of a trapezoid
-var height = prompt("We are calculating the area of a trapezoid.\n Please provide one side of a trapezoid.");
- height=parseInt(height);
- var base = prompt("Please enter in a base for the trapezoid");
- base = parseInt(base);
- var base2 = prompt("Please enter a width for the trapezoid");
- width = parseInt(base2);
+var height = parseInt(prompt("We are calculating the area of a trapezoid.\n Please provide one side of a trapezoid."));
+
+ var base = parseInt(prompt("Please enter in a base for the trapezoid"));
+
+ var base2 = parseInt(prompt("Please enter a width for the trapezoid"));
+
 
  //Array
- var volumeFormula = [height,base,base2];
+ var areaFormula = [height,base,base2];
+
 
 
 //Assignment operators
- var x; // used as variable for formula
+ var x;
+ var y;// used as variable for formula
  var total;
- x = volumeFormula[0] *= volumeFormula[1];
+ x = areaFormula[1]+= areaFormula[2];
+y = x /= 2;
+ total = y * areaFormula[0];
 
- total = x *= volumeFormula[2];
-
+ //total = areaFormula[1]*areaFormula[0]
 console.log(total);
+//console.log(total);
  console.log(alert("The area of your trapezoid is"+" "+total+" "+"Units squared"));
-// (h*b*L)
