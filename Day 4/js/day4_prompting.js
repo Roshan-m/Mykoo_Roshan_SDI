e78a43fa8ce810a27b406666370c8e4b4668fbce
Roshan-m <rjmykoo@fullsail.edu>
2015-01-13 19:56:55 -0500

    Learned about user prompting

--- Day 4/js/day4_prompting.js
+++ Day 4/js/day4_prompting.js
@@ -7,3 +7,25 @@ Prompting
 
 //alert("test");
 
+
+//Ask user for input - prompt();
+// Save the user's response - by creating a variable
+var userInput = prompt("Enter your year of birth");
+console.log(userInput);
+
+//Calculate the area of a rectangle
+// area = length * with
+
+// Create 2 variables and prompt the user for length and with
+var length = prompt("Please entter in length");
+var width = prompt("Please enter in with");
+
+//cakcukate area
+var areaRect = length*width;
+console.log(areaRect);
+
+var length = prompt("Let's calculate the area of a rectangle. \n Please enter in length");
+var width = prompt("Please enter in with");
+
+var areaRect = length * width;
+console.log("The area of your rectangle with a length of"+" "+length+" "+"and a width of"+" "+width+" "+"is"+" "+areaRect);
