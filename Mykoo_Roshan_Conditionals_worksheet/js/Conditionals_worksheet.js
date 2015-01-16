bddbcf9c37a141c50e5fff22edc9c53dcc6260ea
Roshan-m <rjmykoo@fullsail.edu>
2015-01-15 23:10:06 -0500

    question 1 with console logs

--- Mykoo_Roshan_Conditionals_worksheet/js/Conditionals_worksheet.js
+++ Mykoo_Roshan_Conditionals_worksheet/js/Conditionals_worksheet.js
@@ -5,4 +5,22 @@ Conditionals worksheet
 1-15-2015
 */
 
-alert("test");
+//alert("test");
+
+//Last Chance for Gas!
+//A driver has to determine if they can make it across the desert with their current fuel.
+//They are about to past the last gas station for the next 200 miles and they need to determine
+//whether they should stop now for gas or not.
+
+var mpg=prompt("Let's see if your car can make it to the next gas station with out refueling. \nEnter MPG of Vehicle");
+var tankCapacity=prompt("Enter fuel tank capacity.");
+var gasPercent=(prompt("Enter the current fuel level(%)")/100);
+var fuelLeft=(tankCapacity*gasPercent);
+var milesLeftInTank=(fuelLeft*mpg);
+console.log(mpg);
+ console.log(tankCapacity);
+ console.log(gasPercent)
+ //console.log(x);)
+//console.log(fuelLeft);
+//console.log(milesLeftInTank);
+//(milesLeftInTank>200)? console.log("Yes, you can make it without stopping for gas!"):console.log("You only have "+milesLeftInTank+" gallons of gas in your tank, better get gas now while you can!");
