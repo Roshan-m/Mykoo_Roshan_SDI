d2cd3f1f354ff36f9c91b21de263d33bc7efa137
Roshan-m <rjmykoo@fullsail.edu>
2015-01-15 19:18:09 -0500

    Basic if and else
    used boolean

--- Day 5/js/day5Conditional.js
+++ Day 5/js/day5Conditional.js
@@ -29,3 +29,15 @@ Code to run if the condition is true
  }
 
  console.log("What comes after the if statement.");
+
+// If the kid is over 48 inches tall, then he can ride
+ var kidHeight= 30;
+
+ var minimumHeight = 48;
+
+ if(kidHeight> minimumHeight){
+  console.log("You are tall enough to ride!");
+ } else{
+  // This code will run if the test is false
+  console.log("Sorry you are not tall enough!");
+ }
