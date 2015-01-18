48e53d63e0f29e5b130d4df0b1aa5a9018eb170e
Roshan-m <rjmykoo@fullsail.edu>
2015-01-18 17:16:45 -0500

    finished formula

--- Mykoo_Roshan_Conditionals_Assignment/js/mykoo_conditional.js
+++ Mykoo_Roshan_Conditionals_Assignment/js/mykoo_conditional.js
@@ -7,3 +7,26 @@ Conditionals Assignment
 
 //alert("test");
 
+//Can you afford an Xbox One, adding your money saved from  of work and from your last birthday,
+// after you paid your rent?
+
+ var payCheck = parseInt(prompt("Let's see if you can afford and Xbox One!\nPlease enter the amount of money you saved from your last paycheck."));
+ var birthday;
+
+(payCheck == "")? alert("You Did not input part of your information correctly,\n Please refresh and try again "):parseInt(prompt("Please enter the money saved from your birthday."));
+
+
+ var rent = parseInt(prompt("What is your total rent for the month?"));
+
+
+var totalSaved = payCheck  +birthday;
+var moneyToSpend = totalSaved-rent;
+
+var moneyLeft = moneyToSpend-350;
+
+ if(moneyToSpend>350){
+  console.log(alert("You are able to buy an Xbox One with"+" $"+moneyLeft+" "+"to spare!"));
+ }else if (moneyToSpend<350 && moneyToSpend<rent) {
+  console.log(alert("Sorry you cannot buy an Xbox One"));
+ }
+
