8d3dc6b9257b7108a437c4aa97c3a4721eb6222f
Roshan-m <rjmykoo@fullsail.edu>
2015-01-20 18:50:27 -0500

    made for loops

--- Day 7/js/day7forloops.js
+++ Day 7/js/day7forloops.js
@@ -5,3 +5,28 @@ SDI Campus
 For Loops
 */
 //alert("test");
+
+//Basic for loop structure
+/*
+for(initialization; Condition to test: Increment of change){
+    Code to run as long as the condition is true
+    
+}
+*/
+
+for(var i=0; i<20; i+=4){
+    //Code to run as long as i<20
+    console.log("the value of i = "+i);
+    
+}
+
+for(var k=0; k<5; k++){
+    console.log("The Value of k is "+k);
+    //Add a break point
+    //If k is equal to 3 then stop the code
+    
+    if (k===3) {
+        break;
+    }
+    
+}
