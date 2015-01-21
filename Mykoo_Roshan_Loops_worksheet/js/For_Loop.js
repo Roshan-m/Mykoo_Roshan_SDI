6ec3c62752188c1b66173e6f51eaf10e3f394c4e
Roshan-m <rjmykoo@fullsail.edu>
2015-01-21 00:27:59 -0500

    FINISHED ASSIGNMENT
    FINISHED ASSIGNMENT

--- Mykoo_Roshan_Loops_worksheet/js/For_Loop.js
+++ Mykoo_Roshan_Loops_worksheet/js/For_Loop.js
@@ -7,14 +7,24 @@ For Loop
 
 //alert("test");
 
-//1 of Gino's pizza's employee can bake 6 pizzas every hour
-//How many pizzas can be made in a work day with 3 employees
-//baking pizza?
-
-var baker = 3; //3 people baking pizza
-var pizza = 6; //6 pizzas every hour
-var hours = 11; // 11 hours spent baking pizza
+//A salesman hires and recruits 10 other salesmen every hour long seminar he does.
+//All salesmen he hires will also present their own hour long seminars
+//How many salesmen will there be after 6 seminars?
 
 
-for();
+var salesmen = 1;
+var salesRecruits = 10;
+var seminars = 6;
 
+
+for(var semCompleted=1; semCompleted<=seminars; semCompleted+=1){
+
+ var newRecruited = salesmen * salesRecruits;
+
+    salesmen += newRecruited;
+
+ console.log("There "+salesmen+" at the end of "+ semCompleted +" seminars!");
+
+
+
+}
