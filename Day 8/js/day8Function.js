93aa874cd62105e2a88721846e8c81522050733a
Roshan-m <rjmykoo@fullsail.edu>
2015-01-22 18:49:48 -0500

    basic functons

--- Day 8/js/day8Function.js
+++ Day 8/js/day8Function.js
@@ -18,3 +18,24 @@ function printHello(){
 
 //Function call to start our printHello function
 printHello();
+
+printHello();
+
+printMore();
+//Create a function to print more text
+function printMore() {
+    console.log("Inside of print more. Here is more text!");
+}
+
+printMore();
+
+//Create a function that calculates the area of a rectangle
+
+function calcArea() {
+    //Creates variables for width, height and area
+    var width = 20;
+    var height= 30
+    var area = width*height;
+    console.log('The area is '+area);
+}
+calcArea();
