d2c435daa7ec690f5ed6c14cb9146a9cdd43e24b
Roshan-m <rjmykoo@fullsail.edu>
2015-01-22 18:40:23 -0500

    day8 functions html and js

---
+++ Day 8/js/day8Function.js
@@ -0,0 +1,20 @@
+/*
+ Roshan Mykoo
+ 1-22-2015
+ Functions
+ 
+ */
+//alert('test');
+
+//function fuctionName(){code to be run}
+//function call - "go button" = functionName();
+
+//Creating a function that logs on to console "hello"
+
+function printHello(){
+    console.log("hello! you are inside the print hello function!");
+    
+}
+
+//Function call to start our printHello function
+printHello();
